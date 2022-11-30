5359adfe987dc7996fef7ddb66f7be5bde1ba57e
sametKorkmaz <210601699@bakircay.edu.tr>
2022-11-30 20:20:19 +0300

    Add project files.

---
+++ DSProject2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
